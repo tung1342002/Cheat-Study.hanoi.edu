df47765666c2a64ea67dc6369181c4310941316e
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-04-11 12:11:27 +0700

    Create pickRightAnswer.js
    code auto nhập đáp án đúng

---
+++ pickRightAnswer.js
@@ -0,0 +1,16 @@
+//Chon dap an dung (fixed)
+
+var buttons = document.querySelectorAll('input[type="radio"]');
+var hon = JSON.parse(localStorage.getItem("ans"));
+z=0;
+for(i=0; i<buttons.length; i+=4){
+	
+	for(t=i; t<i+4; t++){
+		if(buttons[t].parentNode.innerText == hon[z]){
+			buttons[t].onclick();
+		}else{
+		continue;
+		}
+	}
+	z++;
+}
