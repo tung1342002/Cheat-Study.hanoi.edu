e9499a6335c887b72a2a797a8806d59527c73c0f
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-04-12 21:30:05 +0700

    Create getAnswer.js

---
+++ getAnswer.js
@@ -0,0 +1,11 @@
+var test = document.getElementsByClassName("text-success");
+for(i=2; i<test.length; i+=3){
+	test[i].className = "text-primary";
+}
+f = document.getElementsByClassName("text-primary");
+ans = [];
+for(i=1;i<f.length;i++){
+ var text = " " + f[i].parentNode.innerText;
+ ans.push(text);
+}
+localStorage.setItem("ans",JSON.stringify(ans));
