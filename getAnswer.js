0d94b4dd5e73208d31b99f7ea05146ecab5f14ad
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-04-13 08:19:52 +0700

    Update getAnswer.js

--- getAnswer.js
+++ getAnswer.js
@@ -2,10 +2,10 @@ var test = document.getElementsByClassName("text-success");
 for(i=2; i<test.length; i+=3){
 	test[i].className = "text-primary";
 }
-f = document.getElementsByClassName("text-primary");
+var f = document.getElementsByClassName("text-primary");
 ans = [];
 for(i=1;i<f.length;i++){
- var text = " " + f[i].parentNode.innerText;
- ans.push(text);
+	var text = " " + f[i].parentNode.innerText;
+	ans.push(text);
 }
 localStorage.setItem("ans",JSON.stringify(ans));
