c6832ecddc1ac815bcf837e70ba41c210af4a0cf
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-04-19 12:19:05 +0700

    Update pickRighAnswer.js

--- pickRighAnswer.js
+++ pickRighAnswer.js
@@ -1,7 +1,9 @@
-var buttons = document.querySelectorAll('input[type="radio"]')
-ans = JSON.parse(localStorage.getItem("ans"))
+var buttons = document.querySelectorAll('input[type="radio"]');
+ans = JSON.parse(localStorage.getItem("ans"));
 for(i=0;i<buttons.length;i++){
-    if(ans.includes(buttons[i].parentNode.nextElementSibling.innerText))
-        buttons[i].onclick();
+   for(i2=0;i2<ans.length; i2++){
+      if(buttons[i].parentNode.nextElementSibling.innerText==ans[i2]){
+         buttons[i].onclick();
+      }
+   }
 }
-
